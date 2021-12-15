fb5d38eb9ee4c31b4dda85c33ad7805b7f49001a
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-15 14:22:42 +0100

    add bonus/css

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
-      image: 'natale.jfif'
+      message: 'Happy Christmas!',
+      image: 'natale.jfif',
+      text: 'Buon Natale',
+      image_presents: 'presents.jfif',
+      number : 1
     }
   })
+
+//   Bonus:
+// In una cartella bonus:
+// Inseriamo un input per visualizzare in pagina un
+//  testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che
+//  identifica il numero di prodotti desiderati, i 
+//  dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
